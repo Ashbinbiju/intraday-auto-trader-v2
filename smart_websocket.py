d7614669a666ebb7a0878f9665e39ee509191041
Ashbinbiju <ashbinbijuphilip99@gmail.com>
2026-02-08 01:16:05 +0530

    Fix WebSocket: Send Binary Pong (0x33) for Heartbeat

--- smart_websocket.py
+++ smart_websocket.py
@@ -63,9 +63,9 @@ class OrderUpdateWS:
         if data_type == websocket.ABNF.OPCODE_BINARY:
             # Check for Dhan/EIO Heartbeat: 0x32 (ASCII '2')
             if len(message) > 0 and message[0] == 50: 
-                logger.info("❤️ Heartbeat received (Binary '2'). Sending Pong '3'.")
-                # Send Pong as Text '3' explicitly
-                ws.send("3", opcode=websocket.ABNF.OPCODE_TEXT)
+                logger.info("❤️ Heartbeat received (Binary '2'). Sending Pong b'3'.")
+                # Send Pong as BINARY '3' (0x33) explicitly
+                ws.send(b"3", opcode=websocket.ABNF.OPCODE_BINARY)
                 return
 
         elif data_type == websocket.ABNF.OPCODE_TEXT:
